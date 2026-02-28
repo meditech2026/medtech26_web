c8f46bb510ab3d1e3e77d47ac0cab312830ccbc2
meditech2026 <meditech2026bme@gmail.com>
2026-03-01 00:37:11 +0530

    Delete src directory

--- src/config.js
+++
@@ -1,2 +0,0 @@
-// Replace this URL with your deployed Google Apps Script Web App URL
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby5MBit6-ouLeKfR90lPBpRJYe7wPxbdC8DlU9pDnTZpPAuZ9aCLiw8STbE-O0o69sX/exec";
